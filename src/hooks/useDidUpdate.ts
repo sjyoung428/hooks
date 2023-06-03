5eacfef8e6d869faf096512d55226cb96bd2089a
sjyoung428 <sjy8267@naver.com>
2023-06-04 02:08:34 +0900

    import type lint 적용

--- src/hooks/useDidUpdate.ts
+++ src/hooks/useDidUpdate.ts
@@ -1,4 +1,5 @@
-import { DependencyList, EffectCallback, useEffect, useRef } from "react";
+import type { DependencyList, EffectCallback} from "react";
+import { useEffect, useRef } from "react";
 
 export const useDidUpdate = (effect: EffectCallback, deps: DependencyList) => {
   const hasMounted = useRef(false);
