656aaf3d991d049df5ce1ebd68cf8956ccc6abaf
sjyoung428 <sjy8267@naver.com>
2023-06-03 18:19:53 +0900

    useDebounce useThrottle hook 추가

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+interface useDebounceProps<T> {
+  value: T;
+  delay: number;
+}
+
+export const useDebounce = <T>({ value, delay = 500 }: useDebounceProps<T>) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const debounce = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => clearTimeout(debounce);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
---
+++ src/hooks/useThrottle.ts
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+
+interface useThrottleProps<T> {
+  value: T;
+  delay: number;
+}
+
+export const useTrottle = <T>({ value, delay = 500 }: useThrottleProps<T>) => {
+  const [throttleValue, setTrottleValue] = useState<T>(value);
+  const throttling = useRef(false);
+
+  useEffect(() => {
+    if (throttling.current === false) {
+      setTrottleValue(value);
+      throttling.current = true;
+    }
+
+    setTimeout(() => {
+      if (throttling?.current) {
+        throttling.current = false;
+      }
+    }, delay);
+  }, [value, delay]);
+
+  return throttleValue;
+};
