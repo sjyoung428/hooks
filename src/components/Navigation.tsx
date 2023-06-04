c4bb1e564e201a5660b5087c75f664e91e1a8baa
sjyoung428 <sjy8267@naver.com>
2023-06-04 17:51:06 +0900

    nav style

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import { cls } from "~/utils/cls";
 
 interface NavigationProps {
   to: string;
@@ -8,9 +9,17 @@ interface NavigationProps {
 const Navigation = ({ to, name }: NavigationProps) => {
   const { pathname } = useLocation();
   console.log(pathname);
+
   return (
     <>
-      <li className="">
+      <li
+        className={cls(
+          name.toLocaleLowerCase() === pathname.substring(1) ||
+            (name === "Home" && pathname === "/")
+            ? "underline underline-offset-4 decoration-amber-800"
+            : "no-underline"
+        )}
+      >
         <Link to={to}>{name}</Link>
       </li>
     </>
